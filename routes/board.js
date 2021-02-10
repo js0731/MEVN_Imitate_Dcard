dba15dc7cec24b12677258d77840ee559d963a0d
js0731 <a0905050792@gmail.com>
2021-02-10 20:44:02 +0800

    打包測試

--- routes/board.js
+++ routes/board.js
@@ -4,26 +4,35 @@ const Article = require('../models/Article')
 
 
 router.get('/all/:number', (req, res) => {
+    console.log(req.params.number);
     // Article.find({})
     //     .sort({ love: -1 })
     //     .skip(Number(req.params.number))
     //     .limit(Number(10))
+
     //     .then(articleData => {
-
     //         res.json({ articleData: articleData })
+    // //     })
+    // Article.find({})
+    //     .sort({ love: -1 })
+    //     .then(articleData => {
+    //         articleData.forEach(e => {
+    //             console.log(e.title);
+    //         })
     //     })
-
     Article.find({})
-        .sort({ love: -1 })
-        .skip(Number(req.params.number))
+        .sort({ love: -1, _id: 1 })
         .limit(Number(10))
+        .skip(Number(req.params.number) + 1)
         .then(articleData => {
+            articleData.forEach(e => {
+                console.log(e.title);
+            })
             res.json({ articleData: articleData })
         })
 })
 
 router.get('/all/latest/:number', (req, res) => {
-
     Article.find({})
         .skip(Number(req.params.number))
         .limit(Number(10))
@@ -36,7 +45,7 @@ router.get('/all/latest/:number', (req, res) => {
 router.get('/:board/:number', (req, res) => {
 
     Article.find({ boardPath: req.params.board })
-        .sort({ love: -1 })
+        .sort({ love: -1, _id: 1 })
         .skip(Number(req.params.number))
         .limit(Number(10))
         .then(articleData => {
